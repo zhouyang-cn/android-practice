626aff2ab96fb70c8261fbad0dbd59349ed5954a
zhouyang-cn <lyzhou1107@163.com>
2019-06-16 21:03:50 +0800

    feat: 修改Catalog界面

--- app/src/main/java/com/itzy/practice/CatalogFragment.kt
+++ app/src/main/java/com/itzy/practice/CatalogFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_catalog.*
 
 
 /**
@@ -20,6 +22,13 @@ class CatalogFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_catalog, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btn_20190616.setOnClickListener {
+            findNavController().navigate(R.id.action_catalogFragment_to_customViewsFragment)
+        }
     }
 
 }
